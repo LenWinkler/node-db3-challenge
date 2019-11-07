85cc292482dc39f9ffdf1142fc361175008b5f84
LenWinkler <lenwinkler86@gmail.com>
2019-11-07 01:26:55 -0500

    wrote add, update, and remove helpers but can't get them to return object when they're finished

--- schemes/scheme-model.js
+++ schemes/scheme-model.js
@@ -3,7 +3,10 @@ const db = require('../data/db-Config.js');
 module.exports = {
     find,
     findById,
-    findSteps
+    findSteps,
+    add,
+    update,
+    remove
 };
 
 function find() {
@@ -26,4 +29,23 @@ function findSteps(id) {
     .join('steps', 'schemes.id', 'steps.scheme_id')
     .where({ scheme_id: id })
     .orderBy('step_number');
+}
+
+function add(scheme) {
+    return db('schemes')
+    .returning(['id', 'scheme_name'])
+    .insert(scheme)
+    
+}
+
+function update(changes, id) {
+    return db('schemes')
+    .where({ id })
+    .update({ scheme_name: changes }, ['id', 'scheme_name'])
+}
+
+function remove(id) {
+    return db('schemes')
+    .where({ id })
+    .del()
 }
