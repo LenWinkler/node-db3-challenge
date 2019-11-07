d7910bfc902cd38c7aadb89f3b60b4de1f503cc0
LenWinkler <lenwinkler86@gmail.com>
2019-11-07 00:40:45 -0500

    wrote the 3 'find' helpers

---
+++ schemes/scheme-model.js
@@ -0,0 +1,21 @@
+const db = require('../data/db-Config.js');
+
+module.exports = {
+    find,
+    findById,
+    findSteps
+};
+
+function find() {
+    return db('schemes')
+}
+
+function findById(id) {
+    return db('schemes').where({ id })
+    .first();
+}
+
+function findSteps(id) {
+    return db('steps').where({ scheme_id: id })
+    .orderBy('step_number');
+}
